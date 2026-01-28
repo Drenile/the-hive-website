f2168803cf6968370339f0037b11e0d80f4d5985
Drenile <adeyomot@gmail.com>
2026-01-28 03:53:47 +0000

    -connected supabase to express details for database are contained in .env (do not push to public repo)

---
+++ server/db.js
@@ -0,0 +1,9 @@
+// opens secure connection to database
+const { Pool } = require("pg");
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+module.exports = pool;
--- server/index.js
+++ server/index.js
@@ -1,11 +1,27 @@
 const express = require("express");
-const app = express();
 const cors = require("cors");
+const dotenv = require("dotenv");
+const pool = require("./db");
+
+dotenv.config();
+
+const app = express();
 
 //midlleware
 app.use(cors());
 app.use(express.json());
 
-app.listen (5000, () => {
-    console.log("server has started on port 5000")
+//test route (IMPORTANT)
+app.get("api/test", async (req, res) => {
+    try{
+        const result = await pool.query("SELECT NOW()");
+        res.json(result.rows[0]);
+    } catch (err) {
+        res.status(500).json({ error: err.message});
+    }
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen (PORT, () => {
+    console.log(`server has started on port ${PORT}`);
 });
